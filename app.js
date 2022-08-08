b62dec953049dc2d4979e874ef9f1e83d5d66f1b
Desipeli <93584995+Desipeli@users.noreply.github.com>
2022-08-08 14:41:34 +0300

    eriytetty app

---
+++ app.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+const cors = require('cors')
+const express = require('express')
+const Blog = require('./models/blog')
+
+const app = express()
+
+const mongoUrl = process.env.MONGODB_URI
+mongoose.connect(mongoUrl)
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+module.exports = app
