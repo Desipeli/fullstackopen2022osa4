a5f962569ef516298514b2e2d13218984dd19f7c
Desipeli <93584995+Desipeli@users.noreply.github.com>
2022-08-08 15:11:44 +0300

    router

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const config = require('./utils/config')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const express = require('express')
+const blogsRouter = require('./controllers/blogs')
 const Blog = require('./models/blog')
 
 const app = express()
@@ -11,22 +12,6 @@ mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs',blogsRouter)
 
 module.exports = app
