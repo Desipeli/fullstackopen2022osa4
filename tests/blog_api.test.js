d5d8d76c6a1481238222ae97dad6404ac84a1761
Desipeli <93584995+Desipeli@users.noreply.github.com>
2022-08-09 16:52:03 +0300

    4.8 ja 4.9

---
+++ tests/blog_api.test.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const helper = require('./test_helper')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  const blogObjects = helper.initialBlogs
+    .map(blog => new Blog(blog))
+    const promiseArray = blogObjects.map(blog => blog.save())
+    await Promise.all(promiseArray)
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('correct amount of blogs', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body).toHaveLength(6)
+})
+
+test('blog contains id field', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body[0].id).toBeDefined()
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
---
+++ tests/test_helper.js
@@ -0,0 +1,44 @@
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+  "title": "Titteli",
+  "author": "Autori",
+  "url": "urli",
+  "likes": 10,
+  },
+  {
+  "title": "Titteli2",
+  "author": "Autori2",
+  "url": "urli2",
+  "likes": 20,
+  },
+  {
+  "title": "Titteli3",
+  "author": "Autori3",
+  "url": "urli3",
+  "likes": 30,
+  },
+  {
+  "title": "Titteli4",
+  "author": "Autori4",
+  "url": "urli4",
+  "likes": 40,
+  },
+  {
+  "title": "Titteli5",
+  "author": "Autori5",
+  "url": "urli5",
+  "likes": 50,
+  },
+  {
+  "title": "Titteli6",
+  "author": "Autori6",
+  "url": "urli6",
+  "likes": 60,
+  }
+]
+
+module.exports = {
+  initialBlogs,
+}
