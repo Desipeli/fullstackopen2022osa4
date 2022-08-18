43c376dcd8021291828d0163496c502ce0d920a7
Desipeli <93584995+Desipeli@users.noreply.github.com>
2022-08-18 14:31:48 +0300

    morgan

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const config = require('./utils/config')
+const morgan = require('morgan')
 const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 const cors = require('cors')
@@ -12,6 +13,7 @@ mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
+app.use(morgan('tiny'))
 app.use('/api/blogs',blogsRouter)
 
 
