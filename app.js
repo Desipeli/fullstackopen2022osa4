905990980ebf0993d3611f72a30369a733df2e48
Desipeli <93584995+Desipeli@users.noreply.github.com>
2022-08-18 16:21:31 +0300

    4.15

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const cors = require('cors')
 const express = require('express')
 require('express-async-errors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+
 
 const app = express()
 const mongoUrl = config.MONGODB_URI
@@ -15,6 +17,7 @@ app.use(cors())
 app.use(express.json())
 app.use(morgan('tiny'))
 app.use('/api/blogs',blogsRouter)
+app.use('/api/users', usersRouter)
 
 
 app.use(middleware.unknownEndpoint)
