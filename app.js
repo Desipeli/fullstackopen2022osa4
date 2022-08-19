c118e2ebc4a579f48cca4a4faf53d83538968e71
Desipeli <93584995+Desipeli@users.noreply.github.com>
2022-08-19 11:18:42 +0300

    4.18

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const express = require('express')
 require('express-async-errors')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 
 const app = express()
@@ -18,6 +19,7 @@ app.use(express.json())
 app.use(morgan('tiny'))
 app.use('/api/blogs',blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 
 app.use(middleware.unknownEndpoint)
